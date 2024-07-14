28e07e403a9644f0decf72632700c85489ce3351
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-14 21:34:05 +0530

    Create AssignmentOperator.py

---
+++ 2. Operators In Python/AssignmentOperator.py
@@ -0,0 +1,18 @@
+'''
+Assignment operator : Used for assign the value to variable
+Example : =, +=, -=, /=, %=, 
+'''
+number1 = 3
+print(number1) # 3
+
+number1 += 5 # similar to number1 = number1 + 5
+print(number1) # 8
+
+number1 -=5
+print(number1) # 3
+
+number1/=3  # 1.0
+print(number1)
+
+number1%=2 # 1.0
+print(number1)
