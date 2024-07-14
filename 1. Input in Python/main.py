595a8af9173514a0df375431fe03165fcd7824a8
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-14 21:32:01 +0530

    Create main.py

---
+++ 1. Input in Python/main.py
@@ -0,0 +1,14 @@
+'''
+number1 = input("Enter the number 1: ") # Input taken in only string format
+number2 = input("Enter the number 2: ")
+
+sum = number1 + number2
+print(sum) #output will be "numbe1number2" --> String concatenation happens
+
+# We have to convert explicitely string to required data types
+'''
+
+number1 = int(input("Enter the number 1:")) #converting string to int by explicitely
+number2 = int(input("Enter the number 2:"))
+sum = number1 + number2
+print(sum)
