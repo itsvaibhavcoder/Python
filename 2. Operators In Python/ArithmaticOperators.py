c12ccf5d9f338ff35bfc2ccf9eda0714df574b8d
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-14 21:33:25 +0530

    Create ArithmaticOperators.py

---
+++ 2. Operators In Python/ArithmaticOperators.py
@@ -0,0 +1,21 @@
+'''Arithmatic operators : are used to performed different operation on operand 
+Example : +, -, *, /, //(division floor), % (give remainder), ** (power), 
+'''
+
+number1 = int(input("Enter the number 1 :"))
+number2 = int(input("Enter the number 2 :"))
+
+value1 = number1 + number2 # Output : 5 (number1=3, number2=2)
+value2 = number1 - number2 # Output : 1
+value3 = number1 * number2 # Output : 6
+value4 = number1 / number2 # Output : 1.5
+value5 = number1 // number2 # Output : 1
+value6 = number1 % number2 # Output : 1
+value7 = number1 ** number2 # Output : 9
+print(value1)
+print(value2)
+print(value3)
+print(value4)
+print(value5)
+print(value6)
+print(value7)
