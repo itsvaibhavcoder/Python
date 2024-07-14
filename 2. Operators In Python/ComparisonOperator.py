74b561ad14e84f3662318b7d18621f2bf99424b1
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-14 21:34:43 +0530

    Create ComparisonOperator.py

---
+++ 2. Operators In Python/ComparisonOperator.py
@@ -0,0 +1,12 @@
+'''Comparison operators : comparison operators are used to compare two values and return True and False value
+Example : ==, !=, >, <, >=, <, <=
+'''
+num1 = 3
+num2 = 4
+
+print(num1==num2) # False
+print(num1!=num2) # True
+print(num1>num2) # False
+print(num1>=num2) # False
+print(num1<num2) # True
+print(num1<=num2) # True
